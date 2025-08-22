255f105311f01d82aad8c75f9f602a93d65ade52
Michel Tricot <michel.tricot@gmail.com>
2025-08-21 17:35:43 -0700

    progress

--- server/airbyte_api.js
+++ server/airbyte_api.js
@@ -47,9 +47,9 @@ async function generateWidgetToken(externalUserId) {
                 'Accept': 'application/json'
             },
             body: JSON.stringify({
-                externalUserId: externalUserId,
-                organizationId: process.env.SONAR_AIRBYTE_ORGANIZATION_ID,
-                allowedOrigin: process.env.SONAR_ALLOWED_ORIGIN,
+                external_user_id: externalUserId,
+                organization_id: process.env.SONAR_AIRBYTE_ORGANIZATION_ID,
+                allowed_origin: process.env.SONAR_ALLOWED_ORIGIN,
             })
         });
 
